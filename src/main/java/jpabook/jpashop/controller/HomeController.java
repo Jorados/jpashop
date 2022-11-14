abff95823e04cb813ac3fa185f18d68a4da91fff
Seongjin Jo <seongjin8860@naver.com>
2022-11-14 21:53:28 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.session.SessionConst;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/")
+    public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+}
