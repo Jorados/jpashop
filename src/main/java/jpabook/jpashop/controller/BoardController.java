1ee70a07a07eb09a5ed8f3e8e9f0892a4d7bcb5a
Seongjin Jo <seongjin8860@naver.com>
2022-11-29 17:06:11 +0900

    11.29 17:06

--- src/main/java/jpabook/jpashop/controller/BoardController.java
+++ src/main/java/jpabook/jpashop/controller/BoardController.java
@@ -5,10 +5,13 @@ import jpabook.jpashop.controller.form.BoardForm;
 import jpabook.jpashop.controller.form.itemForm;
 import jpabook.jpashop.domain.Board;
 import jpabook.jpashop.domain.Item;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.repository.BoardSearch;
+import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.BoardService;
+import jpabook.jpashop.service.MemberService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,10 +19,7 @@ import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
@@ -31,24 +31,27 @@ import java.util.List;
 public class BoardController {
 
     private final BoardService boardService;
-
+    private final MemberRepository memberRepository;
+    private final MemberService memberService;
     @GetMapping("/boards")
     public String BoardList(Model model){
-//        List<Board> findBoards = boardService.findAll();
-
-        List<Board> findBoards = boardService.findBoardByMember();
+        List<Board> findBoards = boardService.findAll();
         model.addAttribute("findBoards",findBoards);
         return "boards/boardList";
     }
 
     @GetMapping("/boards/new")
-    public String createBoardForm(Model model){
+    public String createBoardForm(@RequestParam("memberId") Long memberId,Model model){
+        Member findMember = memberService.findOne(memberId);
+        model.addAttribute("findMember",findMember);
         model.addAttribute("boardForm",new BoardForm());
         return "boards/createBoardForm";
     }
 
     @PostMapping("/boards/new")
-    public String createBoard(BoardForm form, BindingResult bindingResult){
+    public String createBoard(@RequestParam("memberId") Long memberId, BoardForm form, BindingResult bindingResult){
+        Member member = memberRepository.findOne(memberId);
+
         if(bindingResult.hasErrors()){
             log.info("bindingResult.hasErrors 글 작성 에러 발생");
             return "boards/createBoardForm";
@@ -56,6 +59,8 @@ public class BoardController {
 
         LocalDateTime now = LocalDateTime.now();
         Board board = new Board();
+        board.setId(form.getId());
+        board.setMember(member);
         board.setName(form.getName());
         board.setContent(form.getContent());
         board.setWriteDate(LocalDateTime.now());
